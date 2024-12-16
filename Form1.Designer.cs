6f35c802e8a357bfeca57f06d49fd6d8648e41e7
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-12-16 13:45:33 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace dateTimePicker1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox2 = new TextBox();
+            dateTimePicker2 = new DateTimePicker();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(89, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Upiši datum leta:";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(393, 53);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 1;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(89, 130);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "Upiši vrijeme leta:";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Time;
+            dateTimePicker2.Location = new Point(393, 130);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.ShowUpDown = true;
+            dateTimePicker2.Size = new Size(300, 31);
+            dateTimePicker2.TabIndex = 3;
+            dateTimePicker2.Value = new DateTime(2024, 12, 16, 0, 0, 0, 0);
+            dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(89, 187);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(613, 198);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(350, 404);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 5;
+            button1.Text = "Rezervacija";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(792, 470);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(textBox2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker2;
+        private RichTextBox richTextBox1;
+        private Button button1;
+
+        RichTextBox 
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace dateTimePicker1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
